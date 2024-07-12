f7199192916539b43edb3e5cb546a747eb520a09
msedeno23 <msedeno.blikon@gmail.com>
2024-07-11 18:28:47 -0600

    Handle GOOGLE_CREDENTIALS_JSON parsing errors
    The code has been updated to load GOOGLE_CREDENTIALS_JSON as a dictionary rather than a string. A catcher for JSONDecodeError has been added to handle parsing errors and log error messages. It also alerts the user when GOOGLE_CREDENTIALS_JSON is not set in environment variables.

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,18 @@ load_dotenv()  # Cargar variables de entorno desde .env
 def create_app():
     app = Flask(__name__)
 
+    # Leer el contenido del JSON desde la variable de entorno
     credentials_json = os.getenv('GOOGLE_CREDENTIALS_JSON')
     if credentials_json:
-        with open('google_credentials.json', 'w') as f:
-            f.write(credentials_json)
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_credentials.json'
+        try:
+            credentials_dict = json.loads(credentials_json)
+            with open('google_credentials.json', 'w') as f:
+                json.dump(credentials_dict, f)
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_credentials.json'
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+    else:
+        print("GOOGLE_CREDENTIALS_JSON not found in environment variables")
 
     with app.app_context():
         from .routes import main
